9ed8d745916d043587012b03befb6b8c51a428c6
jingqiang wu <1375326623@qq.com>
2026-05-24 15:03:11 +0800

    Add files via upload

---
+++ demo-pojo/src/main/java/com/example/demopojo/DemoPojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPojoApplication.class, args);
+    }
+
+}
